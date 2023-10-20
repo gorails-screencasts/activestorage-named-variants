f4e534b22d7d85d2833b830717687dbaae0e2f22
Chris Oliver <excid3@gmail.com>
2023-10-19 19:40:01 -0500

    Initial commit

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,3 +1,7 @@
 class Author < ApplicationRecord
   has_many :posts
+
+  has_one_attached :avatar do |attachable|
+    attachable.variant :thumbnail, resize_to_fill: [100, 100], preprocessed: true
+  end
 end
